fa79ac6573c2eee18b42043948b36c1b4fbb9808
mgiannella <michael.giannella@gmail.com>
2020-04-25 13:57:16 -0400

    Login/Registration fully functional and styled

--- users/forms.py
+++ users/forms.py
@@ -7,12 +7,21 @@ class CreateExtendedUser(forms.ModelForm):
     class Meta:
         model = models.ExtendedUser
         fields = ['email', 'firstname', 'lastname']
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'firstname': forms.TextInput(attrs={'class': "form-control"}),
+            'lastname': forms.TextInput(attrs={'class': 'form-control'})
+        }
 
 
 class CreateOrg(forms.ModelForm):
     class Meta:
         model = models.Organization
         fields = ['email', 'organization_name']
+        widgets = {
+            'email': forms.TextInput(attrs={'class': 'form-control'}),
+            'organization_name': forms.TextInput(attrs={'class': "form-control"})
+        }
 
 
 class UserForm(forms.ModelForm):
---
+++ users/templatetags/custom_tags.py
@@ -0,0 +1,36 @@
+from django import template
+from django.urls import reverse
+
+register = template.Library()
+
+@register.filter('input_type')
+def input_type(ob):
+    '''
+    Extract form field type
+    :param ob: form field
+    :return: string of form field widget type
+    '''
+    return ob.field.widget.__class__.__name__
+
+
+@register.filter(name='add_classes')
+def add_classes(value, arg):
+    '''
+    Add provided classes to form field
+    :param value: form field
+    :param arg: string of classes seperated by ' '
+    :return: edited field
+    '''
+    css_classes = value.field.widget.attrs.get('class', '')
+    # check if class is set or empty and split its content to list (or init list)
+    if css_classes:
+        css_classes = css_classes.split(' ')
+    else:
+        css_classes = []
+    # prepare new classes to list
+    args = arg.split(' ')
+    for a in args:
+        if a not in css_classes:
+            css_classes.append(a)
+    # join back to single string
+    return value.as_widget(attrs={'class': ' '.join(css_classes)})
